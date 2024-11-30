6fe21d3581a98fdcacd1cd3f2e8ce95840e63c85
Albert Kargaev <n00dles7121@gmail.com>
2024-11-29 22:15:24 -0500

    one more commit...

--- ConsoleApp1/Program1.cs
+++ ConsoleApp1/Program1.cs
@@ -1,11 +1,37 @@
 ﻿using System;
-using ConsoleApp1;
+using System.Text;
+using System.IO;
+using System.Globalization;
 
 
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World1!");
 
-Programm2.Main2();
 
-Console.ReadKey();
+namespace ConsoleApp1
+{
+    class Program1
+    {
+        static void Main()
+        {
+            System.Console.WriteLine(Sqrt(5));
+
+
+
+            Console.ReadKey();
+
+        }
+        static public float Sqrt (float num) {
+            if (num < 0) return 0;
+            if (num == 0 || num == 1) return num;
+
+            float result = 0;
+
+            while (result * result < num) {
+                result += 0.000001f;
+            }
+
+
+            return result - 0.000001f;
+        }
+    }
+}
