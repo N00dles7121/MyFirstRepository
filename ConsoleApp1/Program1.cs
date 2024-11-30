86fa222581fbd84168698eceda03eb6ebb0a4914
Albert Kargaev <n00dles7121@gmail.com>
2024-11-29 19:53:50 -0500

    even more changes...

--- ConsoleApp1/Program1.cs
+++ ConsoleApp1/Program1.cs
@@ -1,4 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using ConsoleApp1;
+
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World1!");
+
+Programm2.Main2();
 
 Console.ReadKey();
---
+++ ConsoleApp1/Program2.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+namespace ConsoleApp1
+{
+
+    public class Programm2
+    {
+        static public void Main2()
+        {
+            System.Console.WriteLine("Hello, World2!");
+
+            Console.ReadKey();
+        }
+    }
+}
