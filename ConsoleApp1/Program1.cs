6d82a362fee1f7c3494225befd9166d16649c09e
Albert Kargaev <n00dles7121@gmail.com>
2024-12-04 18:38:15 -0500

    Program to count odd and even numbers in given interval

--- ConsoleApp1/Program1.cs
+++ ConsoleApp1/Program1.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.IO;
 using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 
 
@@ -13,25 +14,84 @@ namespace ConsoleApp1
     {
         static void Main()
         {
-            System.Console.WriteLine(Sqrt(5));
 
+            bool isGoing = true;
 
+            while (isGoing)
+            {
+                GetUserInputs(out int num1, out int num2);
 
-            Console.ReadKey();
+                CheckOddEvenNumbers(num1, num2);
 
-        }
-        static public float Sqrt (float num) {
-            if (num < 0) return 0;
-            if (num == 0 || num == 1) return num;
-
-            float result = 0;
-
-            while (result * result < num) {
-                result += 0.000001f;
+                System.Console.WriteLine("Do you want to continue (Y/N): ");
+                isGoing = Console.ReadLine().ToUpper().Equals("Y");
+                Console.Clear();
             }
 
+            System.Console.WriteLine("Thanks for visiting. Press any key to exit.");
+            Console.ReadKey();
 
-            return result - 0.000001f;
+
+
+
+        }
+
+        static void GetUserInputs(out int x, out int y)
+        {
+            do
+            {
+                Console.WriteLine("Input first number:");
+                int.TryParse(Console.ReadLine(), out int num1);
+                Console.WriteLine("Input second number:");
+                int.TryParse(Console.ReadLine(), out int num2);
+
+                x = num1; y = num2;
+
+                if (x > y)
+                {
+                    Console.WriteLine("Wrong input. First number must be less than second.");
+                    System.Console.WriteLine("--------------------");
+                    continue;
+                }
+            } while (x > y);
+        }
+
+        static void CheckOddEvenNumbers(int left, int right)
+        {
+
+            int evenNumberCount;
+            int oddNumberCount;
+
+            if ((left % 2 != 0 && right % 2 != 0) || (left % 2 == 0 && right % 2 == 0))
+            {
+
+
+
+                if (left % 2 == 0)
+                {
+                    evenNumberCount = (right - left + 1) / 2 + 1;
+                    oddNumberCount = (right - left + 1) / 2;
+
+                    System.Console.WriteLine("Total odd numbers: " + oddNumberCount);
+                    System.Console.WriteLine("Total even numbers: " + evenNumberCount);
+                }
+                else
+                {
+                    oddNumberCount = (right - left + 1) / 2 + 1;
+                    evenNumberCount = (right - left + 1) / 2;
+
+                    System.Console.WriteLine("Total odd numbers: " + oddNumberCount);
+                    System.Console.WriteLine("Total even numbers: " + evenNumberCount);
+                }
+            }
+            else
+            {
+                oddNumberCount = (right - left + 1) / 2;
+                evenNumberCount = oddNumberCount;
+
+                System.Console.WriteLine("Total odd numbers: " + oddNumberCount);
+                System.Console.WriteLine("Total even numbers: " + evenNumberCount);
+            }
         }
     }
 }
